cd92c43898afaa2be5b921b8e8b4a2f9451c934c
Brenda Paz <brendaesquivelpaz@gmail.com>
2023-09-15 19:21:03 -0600

    Ejercicios en Python

---
+++ PerimetroCuadrado.py
@@ -0,0 +1,5 @@
+lado=int(input("Ingresa la medida del lado del cuadrado en cm: "))
+
+perimetro= lado *4 
+
+print("El perímetro del cuadrado son ", perimetro, " cm.")
---
+++ VerificarNumero.py
@@ -0,0 +1,11 @@
+numero=int(input("Ingresa un número para verificar si es positivo, negativo o cero: "))
+
+if numero >=1 :
+   print("El número es positivo")
+
+elif numero <=-1:
+   print("El numero es negativo")
+
+else:
+   print("El numero es cero")
+
